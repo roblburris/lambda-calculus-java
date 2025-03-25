3be88e3aa18cfe3388939d56c75fb6011c6560ec
Robert Burris <roblburris@gmail.com>
2025-03-24 21:22:25 -0700

    beta reduce

--- src/com/roblburris/lambda/calculus/types/Term.java
+++ src/com/roblburris/lambda/calculus/types/Term.java
@@ -22,14 +22,20 @@ public sealed interface Term permits Variable, Abstraction, Application {
         }
     }
 
-    static void substitution(Term t, Term s, int j) {
+    /**
+    * [j -> s]t
+    */
+    static Term substitution(int j, Term s, Term t) {
         return switch (t) {
-            case Variable variable -> variable.index() < cutoff ? variable 
-                : new Variable(variable.index() + dPlace);
-            case Abstraction abs -> new Abstraction(shift(abs.m(),
-                dPlace, cutoff + 1));
-            case Apply apply -> new Apply(shift(apply.m(), dPlace, cutoff),
-                shift(apply.n(), dPlace, cutoff));
+            case Variable variable -> variable.index()  == j ? s 
+                : t;
+            case Abstraction abs -> new Abstraction(
+                substitution(j+1, shift(s, 1, 0), abs.m()));
+            case Apply apply -> new Apply(substitution(j, s, apply.m()), substitution(j, s, apply.n()));
         }
+    }
+
+    static Term betaReduce(Abstraction abs, Variable v) {
+        return shift(substitution(0, v, abs), -1, 0);
     }
 }
